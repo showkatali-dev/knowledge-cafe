7bf6e592e81dd143ae7fbe3b527849b3718089ee
coder-showkat <showkat.coder@gmail.com>
2023-03-30 20:49:11 +0600

    adding utilities

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-green-400 text-center text-3xl font-semibold">App</div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        myShadow1: '2px -5px 1px #111827',
+        myShadow2: '-2px -5px 1px #111827',
+      }
+    },
+  },
+  plugins: [],
+}
